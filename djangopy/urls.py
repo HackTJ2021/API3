9cc6d7a758e55d5a14319de696456da2c05762fd
Ram Reddy <ram.n.reddy15@gmail.com>
2021-04-10 17:20:29 -0400

    new stuff

--- djangopy/urls.py
+++ djangopy/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.views.generic import TemplateView
+from django.urls import path, include
 
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('', include('app.urls')),
+    path('',  TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
-]
+    path('api/', include('mainAPI.urls')),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
