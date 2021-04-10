50f8fa8b1aa51e157dbcdf6167f7127f375b22d1
Ram Reddy <ram.n.reddy15@gmail.com>
2021-04-10 17:18:51 -0400

    new stuff

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin
+from .models import MyFile
 
-# Register your models here.
+admin.site.register(MyFile)
--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
+from .views import ImageRecognition
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('image/', ImageRecognition.as_view()),
 ]
